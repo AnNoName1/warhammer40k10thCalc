93fdd9491ed880ce11e963ad95a52d31dc3f607d
AnNoName1 <olbutovalekdsandr@gmail.com>
2026-02-11 23:28:36 +0300

    chore(license): apply missing MIT license headers to project files
    WHY:
    - Previous pre-push hook failed as intended because license headers were missing.
    - This commit stages the changes generated by 'addlicense' to satisfy the hook's clean-state requirement.
    
    ATOMICITY:
    - Atomic: yes
    
    TRANSITIONAL CODE:
    - Transitional code introduced: no
    
    TESTS:
    - Tests added in this commit:
      - [ ] happy-path
      - [ ] error cases
      - [x] invariants / properties: Header presence is now an invariant enforced by pre-push logic.
    
    POLICY EXCEPTION:
    - Required: no

--- internal/calculator/core_test.go
+++ internal/calculator/core_test.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2026 Olbutov Aleksandr
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 package calculator
 
 import (
@@ -414,13 +434,13 @@ func TestCalculateDamageCore_Torrent(t *testing.T) {
 				verifyDist(t, "HitDist", resp.HitDist, tt.expectedHitsDist)
 			}
 			if tt.expectedKilledDist != nil {
-                verifyDist(t, "DestroyedDist", resp.DestroyedDist, tt.expectedKilledDist)
-            }
-            if tt.expectedDamageDist != nil { 
-                verifyDist(t, "DamageDist", resp.DamageDist, tt.expectedDamageDist)
-            }
-        })
-    } 
+				verifyDist(t, "DestroyedDist", resp.DestroyedDist, tt.expectedKilledDist)
+			}
+			if tt.expectedDamageDist != nil {
+				verifyDist(t, "DamageDist", resp.DamageDist, tt.expectedDamageDist)
+			}
+		})
+	}
 }
 
 func intPtr(i int) *int {
